843134d3b78ada4a89bba4cbe1d93f7dfb062f92
Yurii Bosov <bosov.yu@gmail.com>
2017-06-11 11:06:47 +0300

    add MyViewController

---
+++ GIT_1/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  GIT_1
+//
+//  Created by Yurii Bosov on 6/11/17.
+//  Copyright © 2017 Yurii Bosov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
